74fc8e822a79bb708f3793cf83a76d6e1c3684e0
Akhil Potla <Akhil Potla>
2016-06-14 14:44:43 -0500

    Added helper method to read data from file and create map between country code and life expectancy.

--- src/guimodule/LifeExpectancy.java
+++ src/guimodule/LifeExpectancy.java
@@ -6,19 +6,46 @@ import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
+import java.awt.List;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LifeExpectancy extends PApplet{
 
 	UnfoldingMap map;
+	Map<String, Float> lifeExpByCountry;
+
 	
 	public void setup()
 	{
 		size(800, 600, OPENGL);
 		map = new UnfoldingMap(this, 50, 50	, 700, 500, new Google.GoogleMapProvider());
 		MapUtils.createDefaultEventDispatcher(this, map);
+		
+		lifeExpByCountry = loadLifeExpectancyFromCSV("data/LifeExpectancyWorldBank.csv");
 	}
 	
 	public void draw()
 	{
 		map.draw();
+	}
+	
+	private Map<String, Float> loadLifeExpectancyFromCSV(String fileName)
+	{
+		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+		String[] rows = loadStrings(fileName);
+		
+		for(String row : rows)
+		{
+			String[] columns = row.split(",");
+		//	if () 
+	//		{
+				float value = Float.parseFloat(columns[4]);
+				lifeExpMap.put(columns[3], value);
+	//		}
+			
+		}
+		
+		return lifeExpMap;
 	}
 }
